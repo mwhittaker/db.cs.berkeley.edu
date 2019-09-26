96ca42f4c88ef38ee7225efb114bc4235e38c40e
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-09-26 14:55:13 -0700

    Set up university logos.

---
+++ dbgroup.js
@@ -0,0 +1,37 @@
+function setup_university_logos() {
+  const perViewFromWidth = (screen_width) => {
+    // The .center element is 700px wide. Every logo is 75px wide. So, we divide
+    // 700 by 75 to see how many logos will fit and subtract 1 to give it a bit
+    // of space.
+    const width = Math.min(screen_width, 700);
+    return Math.max(1, Math.floor(width / 75) - 1);
+  }
+
+  const glide = new Glide('#university-logos', {
+    type: 'carousel',
+    perView: perViewFromWidth(window.screen.width),
+    autoplay: 2000,
+    hoverpause: true,
+    keyboard: false,
+  });
+
+  glide.on('resize', function() {
+    glide.update({perView: perViewFromWidth(window.screen.width)});
+  });
+
+  glide.mount();
+}
+
+function setup_company_logos() {
+}
+
+function setup_startup_logos() {
+}
+
+function main() {
+  setup_university_logos();
+  setup_company_logos();
+  setup_startup_logos();
+}
+
+window.onload = main;
