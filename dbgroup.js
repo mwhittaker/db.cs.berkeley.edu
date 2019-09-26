b14668b301a80b059dc79ab776a1403770c11036
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-09-26 15:52:58 -0700

    Reduced duplicated code.

--- dbgroup.js
+++ dbgroup.js
@@ -1,79 +1,34 @@
-function setup_university_logos() {
-  const perViewFromWidth = (screen_width) => {
-    // The .center element is 700px wide. Every logo is 75px wide. So, we divide
-    // 700 by 75 to see how many logos will fit and subtract 1 to give it a bit
-    // of space.
-    const width = Math.min(screen_width, 700);
-    return Math.max(1, Math.floor(width / 75) - 1);
-  }
-
-  const glide = new Glide('#university-logos', {
-    type: 'carousel',
-    perView: perViewFromWidth(window.screen.width),
-    autoplay: 2000,
-    hoverpause: true,
-    keyboard: false,
-  });
-
-  glide.on('resize', function() {
-    glide.update({perView: perViewFromWidth(window.screen.width)});
-  });
-
-  glide.mount();
+// A carousel can display `perView` logos at once. The number of logos that we
+// decide to show depends on the size of the screen. If the screen is big, we
+// can show a lot. If the screen is small, we can only show a few.
+// `perViewFromWidth` takes in width of each logo and spits out a number of
+// logos that looks good on the given screen size.
+function perViewFromWidth(logo_width) {
+  // The .center element is 700px wide.
+  const width = Math.min(window.screen.width, 700);
+  return Math.max(1, Math.floor(width / logo_width) - 1);
 }
 
-function setup_company_logos() {
-  const perViewFromWidth = (screen_width) => {
-    // The .center element is 700px wide. Every logo is 150px wide. So, we
-    // divide 700 by 150 to see how many logos will fit and subtract 1 to give
-    // it a bit of space.
-    const width = Math.min(screen_width, 700);
-    return Math.max(1, Math.floor(width / 150) - 1);
-  }
-
-  const glide = new Glide('#company-logos', {
+function setup_carousel(id, logo_width) {
+  const glide = new Glide(id, {
     type: 'carousel',
-    perView: perViewFromWidth(window.screen.width),
+    perView: perViewFromWidth(logo_width),
     autoplay: 2000,
     hoverpause: true,
     keyboard: false,
   });
 
   glide.on('resize', function() {
-    glide.update({perView: perViewFromWidth(window.screen.width)});
-  });
-
-  glide.mount();
-}
-
-function setup_startup_logos() {
-  const perViewFromWidth = (screen_width) => {
-    // The .center element is 700px wide. Every logo is 150px wide. So, we
-    // divide 700 by 100 to see how many logos will fit and subtract 1 to give
-    // it a bit of space.
-    const width = Math.min(screen_width, 700);
-    return Math.max(1, Math.floor(width / 150) - 1);
-  }
-
-  const glide = new Glide('#startup-logos', {
-    type: 'carousel',
-    perView: perViewFromWidth(window.screen.width),
-    autoplay: 2000,
-    hoverpause: true,
-    keyboard: false,
-  });
-
-  glide.on('resize', function() {
-    glide.update({perView: perViewFromWidth(window.screen.width)});
+    glide.update({perView: perViewFromWidth(logo_width)});
   });
 
   glide.mount();
 }
 
 function main() {
-  setup_university_logos();
-  setup_company_logos();
-  setup_startup_logos();
+  setup_carousel('#university-logos', 75);
+  setup_carousel('#company-logos', 150);
+  setup_carousel('#startup-logos', 150);
 }
 
 window.onload = main;
